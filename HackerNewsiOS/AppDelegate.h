5f883cf91169a013261d05573c44dc2488358f35
Qiki <qi@brandingbrand.com>
2015-10-18 15:51:38 -0400

    Initial Commit

---
+++ HackerNewsiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackerNewsiOS
+//
+//  Created by kiki on 10/18/15.
+//  Copyright © 2015 kiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HackerNewsiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackerNewsiOS
+//
+//  Created by kiki on 10/18/15.
+//  Copyright © 2015 kiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
